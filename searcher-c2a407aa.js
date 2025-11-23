cad351476850ee27054652136ec31cbe1c1302ac
daveads <daveads@users.noreply.github.com>
2025-11-23 05:42:23 +0000

    deploy: 459c25bf6734ff7e0b592a6d6eb576210481c51c

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-65229004.js',
+                path_to_root + 'searchindex-7f63b97b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
